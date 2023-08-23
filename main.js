ca60b9afad1c922eb1abd68aaa2dc27786958460
ChobaConejito <130503024+ChobaConejito@users.noreply.github.com>
2023-08-22 20:40:13 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+background('#FFC0CB');
+
+  document.getElementById("font_size").innerHTML = "El unitamaño de la unifuente es: = " + difference +"px";
+textSize(difference);
+fill('#FF69B4');
+text('🦄Unicornio🦄', 50, 400);
+}
+
+
+ 
+
